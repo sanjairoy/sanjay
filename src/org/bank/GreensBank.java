c434cf8b6a16e5a7af06a9a1cd3bb142346db916
sanjairoy <sanjairoy112@gmail.com>
2021-10-28 12:34:14 +0530

    commit

---
+++ src/org/bank/GreensBank.java
@@ -0,0 +1,23 @@
+package org.bank;
+
+public class GreensBank {
+	public static void main(String[] args) {
+		int a = 10, b=20, c= 50;
+				if (a>b) {
+					System.out.println("a is greater");
+					
+				}
+				else if (b>c) {
+					System.out.println("b is grester");
+					
+				}
+				else if (c>a) {
+					System.out.println("a is greater than all numbers");
+				}
+
+				}
+				
+	}	
+	
+
+
